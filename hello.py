aada9175f75f1fa515859e8d2aada8b9fae15851
JulesKouatchou <jrkouatchou@gmail.com>
2023-05-26 17:31:10 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
 print("Hello World!")
 print("\t This is my first Git repository on GitHib.")
+
+def print_message(my_name='Python'):
+    print(f"Welcome to the class from {my_name}!)
+          
+name = "Julia"
+print_message(name)
